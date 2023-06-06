be675d4cee3e67406b6f7f9e6245214a23f4a409
Tagun_py04@top-academy.ru <Tagun_py04@top-academy.ru>
2023-06-06 17:38:08 +0300

    done

--- HomeWork 1.1/Program.cs
+++ HomeWork 1.1/Program.cs
@@ -10,6 +10,27 @@ namespace HomeWork_1._1
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Введите целое число в диапазоне от 0 до 100 :  ");
+            string numberString = Console.ReadLine();
+            int number;
+            number = Convert.ToInt32(numberString);
+            if (number < 0 || number > 100)
+            {
+                Console.WriteLine("Error");
+            }
+            else if (number % 3 == 0 && number % 5 == 0)
+            {
+                Console.WriteLine("Fizz Buzz");
+            }
+            else if (number % 3 == 0)
+            {
+                Console.WriteLine("Fizz");
+            }
+            else if (number % 5 == 0)
+            {
+                Console.WriteLine("Buzz");
+            }
+            Console.ReadLine();
         }
     }
 }
